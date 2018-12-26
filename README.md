185821eb514af0341d833e1a45400c4eefffb4c5
Luis Herranz <luisherranz@gmail.com>
2018-12-26 11:48:56 +0100

    Initial commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# cs.frontity.io
-Created with CodeSandbox
---
+++ src/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./styles.css";
+
+const Link = ({ name, href }) => (
+  <div className="link">
+    <a href={href} target="_blank">
+      {name}
+    </a>
+  </div>
+);
+
+const App = () => (
+  <div className="App">
+    <h1>Customer Success tools for Frontity</h1>
+    <Link name="Priority System" href="https://priorities.frontity.io/" />
+    <Link
+      name="URL directory for Github Issues"
+      href="https://docs.google.com/spreadsheets/d/1FWcvX7KiLAppGW2qOICxs8ATWqV1uEsCVdTnq5mK7q8/edit#gid=0"
+    />
+    <Link name="Autodemos" href="https://autodemos.frontity.io/" />
+    <Link name="Sites Info" href="https://sites-info.frontity.io/" />
+  </div>
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
