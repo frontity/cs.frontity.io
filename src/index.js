e366b0ab13999efb916e90ce496d33f6961975df
Luis Herranz <luisherranz@gmail.com>
2018-12-26 11:51:02 +0100

    build(links): don't use end slash

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ const Link = ({ name, href }) => (
 const App = () => (
   <div className="App">
     <h1>Customer Success tools for Frontity</h1>
-    <Link name="Priority System" href="https://priorities.frontity.io/" />
+    <Link name="Priority System" href="https://priorities.frontity.io" />
     <Link
       name="URL directory for Github Issues"
       href="https://docs.google.com/spreadsheets/d/1FWcvX7KiLAppGW2qOICxs8ATWqV1uEsCVdTnq5mK7q8/edit#gid=0"
     />
-    <Link name="Autodemos" href="https://autodemos.frontity.io/" />
-    <Link name="Sites Info" href="https://sites-info.frontity.io/" />
+    <Link name="Autodemos" href="https://autodemos.frontity.io" />
+    <Link name="Sites Info" href="https://sites-info.frontity.io" />
   </div>
 );
 
